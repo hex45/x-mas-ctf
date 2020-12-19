7f0d6c486dab7375e82c51d739370ec261fedd50
root <root@localhost.localdomain>
2020-12-18 21:00:35 -0500

    tweak

--- dump_volatility_files.py
+++ dump_volatility_files.py
@@ -8,11 +8,11 @@ import os
 input_filename = "Johnbi.txt"
 
 command = 'volatility --profile=Win7SP1x64 dumpfiles -f destroyed.elf -D dump/ --name file -Q '
-phoffset = []
 
 with open(input_filename, "r") as in_file:
     input_data = in_file.readlines()
 
+    phoffset = ''
     for line in input_data:
         phoffset = line.split(' ')[0].strip()
         print(f'processing {phoffset}')
