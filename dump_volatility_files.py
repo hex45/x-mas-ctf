53b2a7bc8d66b91bdf4966cd9aa02ce1e3a7e7f9
root <root@localhost.localdomain>
2020-12-18 20:53:52 -0500

    script for dumping files from elf core dump

---
+++ dump_volatility_files.py
@@ -0,0 +1,19 @@
+
+
+"""
+takes a list of volatility dumpfiles and gets the -Q 
+"""
+import os
+
+input_filename = "Johnbi.txt"
+
+command = 'volatility --profile=Win7SP1x64 dumpfiles -f destroyed.elf -D dump/ --name file -Q '
+phoffset = []
+
+with open(input_filename, "r") as in_file:
+    input_data = in_file.readlines()
+
+    for line in input_data:
+        phoffset = line.split(' ')[0].strip()
+        print(f'processing {phoffset}')
+        os.system(f'{command}{phoffset}')
